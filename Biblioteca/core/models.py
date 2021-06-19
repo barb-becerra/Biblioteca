d4aee7fbe9c4dff6085b6ef8fab7ff0522bfd425
Barbara Becerra <barb.becerra@duocuc.cl>
2021-06-19 18:00:27 -0400

    models arreglos

--- Biblioteca/core/models.py
+++ Biblioteca/core/models.py
@@ -7,7 +7,7 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombreCategoria
 class Libro(models.Model):
-    idISBN = models.IntegerField(primary_key=True, verbose_name='Id de ISBN')
+    idISBN = models.CharField(max_length=13, primary_key=True, verbose_name='Id de ISBN')
     nombreLibro = models.CharField(max_length = 50, verbose_name='Nombre del Libro')
     autorLibro = models.CharField(max_length = 50, verbose_name='Nombre del Autor')
     descripcion = models.CharField(max_length = 100, verbose_name = 'Descripcion del libro')
