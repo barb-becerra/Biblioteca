38670c5e43231a6ae1229326d74a7591491983f1
Matias Iglesias <mat.iglesias@duocuc.cl>
2021-06-19 19:34:07 -0400

    cambios

--- Biblioteca/core/urls.py
+++ Biblioteca/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import inicio, formulario
+from .views import inicio, formulario, libros, form_libro
 
 urlpatterns = [
     path('', inicio, name="inicio"),
     path('formulario', formulario, name="formulario"),
+    path('libros', libros, name="libros"),
+    path('addlibro', form_libro, name="form_libro"),
 ]
 
--- Biblioteca/core/views.py
+++ Biblioteca/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Libro
+#from .forms import LibroForm
 
 # Create your views here.
 def inicio(request):
@@ -6,3 +8,14 @@ def inicio(request):
     
 def formulario(request):
     return render(request, 'core/formulario.html')
+
+def libros(request):
+    libros = Libro.objects.all()
+    datos = {
+        'libros' : libros
+    }
+    return render(request, 'core/libros.html', datos)
+
+def form_libro(request):
+    return render(request, 'core/addlibro.html')
+
