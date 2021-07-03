e33ac255992c8d6f0f285f78c2342198ac62dc64
Matias Iglesias <mat.iglesias@duocuc.cl>
2021-07-03 18:23:29 -0400

    Merge branch 'master' of https://github.com/barb-becerra/Biblioteca

--- Biblioteca/rest_libro/urls.py
+++ Biblioteca/rest_libro/urls.py
@@ -3,4 +3,5 @@ from rest_libro.views import lista_libros
 
 urlpatterns = [
     path('lista_libros', lista_libros, name="lista_libros"),
+    path('detalle_libro/<id>', detalle_libro, name="detalle_libro"),
 ]
