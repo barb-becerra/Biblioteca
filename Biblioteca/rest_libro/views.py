c674820eed6c56fc003a4289715b49542e69b000
Barbara Becerra <barb.becerra@duocuc.cl>
2021-07-03 18:19:20 -0400

    delete y put

--- Biblioteca/rest_libro/views.py
+++ Biblioteca/rest_libro/views.py
@@ -21,3 +21,25 @@ def lista_libros(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT', 'DELETE'])
+def detalle_libro(request, id):
+    try:
+        libro = Libro.objects.GET(idISBN=id)
+    except Libro.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method== 'GET':
+        serializer = LibroSerializer(libro)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = LibroSerializer(libro, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        libro.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
